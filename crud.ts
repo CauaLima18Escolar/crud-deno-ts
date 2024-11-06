198ddf69c9b837cd0c9aadd5aab11759e5bdb782
Cauã de Lima <caua-l1@outlook.com>
2024-11-06 11:38:19 -0300

    ajustando funcionalidade updateST e deleteST

--- crud.ts
+++ crud.ts
@@ -12,25 +12,31 @@ async function readData() {
   return DATA ? JSON.parse(DATA) : null;
 }
 
-async function writeFile(ST_DATA:object) {
+async function writeFile(ST_DATA:object, isDeleting?:boolean) {
     const CURRENT_DATA = await readData()
 
-    if (CURRENT_DATA) {
-        const DATA = Object.assign({}, CURRENT_DATA, ST_DATA)
-        await Deno.writeTextFile(FILE_PATH, JSON.stringify(DATA, null, 2));
+    if (CURRENT_DATA && !isDeleting) {
+      const DATA = Object.assign({}, CURRENT_DATA, ST_DATA)
+      await Deno.writeTextFile(FILE_PATH, JSON.stringify(DATA, null, 2));
+      return
 
-    } else {
-      await Deno.writeTextFile(FILE_PATH, JSON.stringify(ST_DATA, null, 2));
-    }
+    } 
     
-    console.log('Estudante adicionado')
+    if (isDeleting) {
+      await Deno.writeTextFile(FILE_PATH, JSON.stringify(ST_DATA, null, 2));
+      return
+    }
+
+    await Deno.writeTextFile(FILE_PATH, JSON.stringify(ST_DATA, null, 2));
+    return
 }
 
 // Adicionar um estudante ao JSON
-async function createST(mat:string, stName:string, stAge:number, stCourse:string) {
-  STUDENT.set(mat, { name: stName, age: stAge, course: stCourse})
+async function createST(MAT:string, stName:string, stAge:number, stCourse:string) {
+  STUDENT.set(MAT, { name: stName, age: stAge, course: stCourse})
   const ST_DATA = Object.fromEntries(STUDENT)
   await writeFile(ST_DATA)
+  console.log('Estudante adicionado')
 }
 
 // Mostrar estudantes no JSON
@@ -48,49 +54,81 @@ async function readST() {
 }
 
 // Atualizar um estudante no JSON
-async function updateST(mat:string) {
+async function updateST(MAT:string) {
   const DATA = await readData()
-  const ST_DATA = DATA[mat]
+  const ST_DATA = DATA[MAT]
   const OPTIONS = `
 
-  [1] Para mudar o nome do estudante
-  [2] Para mudar a idade do estudante
-  [3] Para mudar o curso do estudante
+  [1] Para mudar a matrícula do estudante
+  [2] Para mudar o nome do estudante
+  [3] Para mudar a idade do estudante
+  [4] Para mudar o curso do estudante
 
   `
   if (ST_DATA) {
     console.log(ST_DATA, OPTIONS)
     const OPTION:number = parseInt(prompt('INSIRA UMA OPÇÃO >>> ') || '0')
 
+    let NEW_ST_DATA:object
+
     switch(OPTION) {
       case 1:
+        const NEW_MAT:string = prompt('INFORME A NOVA MATRÍCULA >>> ') || `${MAT}`
+        NEW_ST_DATA = { [NEW_MAT]: ST_DATA }
+        await deleteST(MAT)
+        await writeFile(NEW_ST_DATA)
+        break
+      case 2:
         const NEW_NAME:string = prompt('INFORME O NOVO NOME >>> ') || `${ST_DATA.name}`
         ST_DATA.name = NEW_NAME
-        await writeFile(ST_DATA)
+        NEW_ST_DATA = { [MAT]: ST_DATA }
+        await writeFile(NEW_ST_DATA)
+        break
+
+      case 3:
+        const NEW_AGE:number = parseInt(prompt('INFORME A NOVA IDADE >>> ') || `${ST_DATA.age}`)
+        ST_DATA.age = NEW_AGE
+        NEW_ST_DATA = { [MAT]: ST_DATA }
+        await writeFile(NEW_ST_DATA)
+        break
+
+      case 4:
+        const NEW_COURSE:string = prompt('INFORME A NOVA IDADE >>> ') || `${ST_DATA.course}`
+        ST_DATA.course = NEW_COURSE
+        NEW_ST_DATA = { [MAT]: ST_DATA }
+        await writeFile(NEW_ST_DATA)
         break
         
       default:
-        console.log('ESSA OPÇÃO NÃO EXISTE.')
+        console.log('Erro: Essa opção não existe.')
         break
     }
+
+    return
   }
 
   return console.log('Estudante não encontrado')
 }
 
+async function deleteST(MAT:string) {
+  const DATA = await readData()
+
+  if (DATA[MAT]) {
+    delete DATA[MAT]
+    await writeFile(DATA, true)
+    return console.log('Estudante deletado.')
+  }
+
+  return console.log('Não há estudante com essa matrícula.')
+}
+
 function main() {
   const args = Deno.args;
-  const commands = ['createST', 'readST', 'updateST', 'deleteST']
 
   let mat:string
   let stName:string
   let stAge:number
   let stCourse:string
-
-  if (!commands.includes(args[0])) {
-    console.log("Erro: Esse comando não existe no CRUD");
-    return;
-  }
 
   switch(args[0]) {
     case 'createST':
@@ -118,6 +156,19 @@ function main() {
       mat = args[1];
 
       updateST(mat)
+      break
+    case 'deleteST':
+      if (args.length !== 2) {
+        console.log("Erro. Esse comando precisa de um argumento.");
+        return;
+      }
+
+      mat = args[1];
+
+      deleteST(mat)
+      break
+    default:
+      console.log("Erro: Esse comando não existe no CRUD");
       break
   }
 }
